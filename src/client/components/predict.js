bcede121e901ea34b5932e53f56982566993ad8e
Parveen <parveen.kumar@tothenew.com>
2017-10-07 22:21:13 +0530

    company info changes

--- src/client/components/predict.js
+++ src/client/components/predict.js
@@ -1,55 +1,65 @@
 import React, {Component, PropTypes} from 'react'
 import {Form} from 'semantic-ui-react'
 import GoToHome, {getData} from './../utils/'
-import { browserHistory} from 'react-router'
+import {browserHistory, Link} from 'react-router'
 
 
 class Predict extends Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            companyInfo: {}
+        }
     }
 
 
-    getAPIData =() => {
-      getData((data) =>{
-        console.log("API response", data);
-        this.gotoScore(data)
-      })
+    getAPIData = () => {
+        getData((data) => {
+            console.log("API response", data);
+            this.gotoScore(data)
+        })
     }
-    gotoScore =(data) => {
-      console.log("LSLLSLS", data);
-      browserHistory.push({pathname: '/score', query: {"data": data.data.userId } })
+    gotoScore = (data) => {
+        browserHistory.push({pathname: '/score', query: {"data": data.data.userId}})
     }
 
-    handleChange = (e, {value}) => this.setState({value})
+    handleChange = (e, {value, name}) => {
+        this.setState({companyInfo: {name: value}})
+    }
+
 
     render() {
         return (
             <div>
                 <h1 className="ui header">Predict Screen</h1>
                 <GoToHome className="ui_header"></GoToHome>
-                <button className="ui ui_header violet button" role="button" onClick={() => this.getAPIData()}>Get API Response</button>
-                <button className="ui ui_header violet button" role="button" onClick={() => this.gotoScore()}>Go To Score</button>
+                <button className="ui ui_header violet button" role="button" onClick={() => this.getAPIData()}>Get API
+                    Response
+                </button>
+                <button className="ui ui_header violet button" role="button" onClick={() => this.gotoScore()}>Go To
+                    Score
+                </button>
                 <div className="company_form">
-                <Form>
-                    <Form.Group widths='equal'>
-                        <Form.Input label='Company name' placeholder='Company name'/>
-                        <Form.Input label='Company size' placeholder='Company size'/>
-                        <Form.Input label='Tech Stack' placeholder='Tech Stack'/>
-                    </Form.Group>
-                    <Form.Group widths='equal'>
-                        <Form.Input label='Founded Year' placeholder='Year Founded'/>
-                        <Form.Input label='Contact Person' placeholder='Enter contacting person'/>
-                        <Form.Input label='Specialties' placeholder='Specialties'/>
-                    </Form.Group>
-                    <Form.Input label='Company Website' placeholder='Company Website..'/>
-                    <div className="center">
-                        <Form.Button>Predict</Form.Button>
-                    </div>
-                </Form>
+                    <Form>
+                        <Form.Group widths='equal'>
+                            <Form.Input name="companyName" onChange={this.handleChange} label='Company name'
+                                        placeholder='Company name'/>
+                            <Link to="chart" target="_blank" to={`https://in.linkedin.com/company/${this.state.companyInfo.name}`}>Test</Link>
+                            <Form.Input label='Company size' placeholder='Company size'/>
+                            <Form.Input label='Tech Stack' placeholder='Tech Stack'/>
+                        </Form.Group>
+                        <Form.Group widths='equal'>
+                            <Form.Input label='Founded Year' placeholder='Year Founded'/>
+                            <Form.Input label='Contact Person' placeholder='Enter contacting person'/>
+                            <Form.Input label='Specialties' placeholder='Specialties'/>
+                        </Form.Group>
+                        <Form.Input label='Company Website' placeholder='Company Website..'/>
+                        <div className="center">
+                            <Form.Button>Predict</Form.Button>
+                        </div>
+                    </Form>
+                </div>
             </div>
-        </div>
         )
     }
 }
