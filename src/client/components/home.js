dcdeb3144cf3d451de6b38e644f5b9d01d6f63f8
dipesh singh <dipesh.singh2012@gmail.com>
2017-10-07 15:14:28 +0530

    Fix merge conflicts

--- src/client/components/home.js
+++ src/client/components/home.js
@@ -1,8 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import ReactDOM from 'react-dom'
 import { browserHistory } from 'react-router';
-import Introbox from './Introbox'
-import Footer from './Footer'
+import Introbox from './introbox'
+import Footer from './footer'
 import img from './img.png'
 
 class Dashboard extends Component {
