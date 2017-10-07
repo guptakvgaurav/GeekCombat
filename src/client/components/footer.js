7de91b78133154365e68e118a5d4c806aa3f23ac
yetinbmf <yatendra.bhardwaj@tothenew.com>
2017-10-07 14:46:29 +0530

    Home Page tengretion with react

---
+++ src/client/components/footer.js
@@ -0,0 +1,18 @@
+import React, {Component, PropTypes} from 'react'
+import GoToHome from './../utils/'
+import {Form} from 'semantic-ui-react'
+
+class Footer extends Component {    
+
+    render() {
+        return (
+        <div className="csm-footer-wrapper">
+            <div className="wrapper">		
+            </div>
+          </div>
+        )
+    }
+}
+
+
+export default Footer
